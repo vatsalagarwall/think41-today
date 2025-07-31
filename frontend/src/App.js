73ac8b31e5a7de3f58eff3ef03d6a378cb0fb960
Vatsal Agarwal <103558712+vatsalagarwall@users.noreply.github.com>
2025-07-31 11:52:31 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList.js';
+import ProductDetail from './components/ProductDetail';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
