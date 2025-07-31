8eb79712ef7f3d1cc848bed7b8b979baa49de9ba
Vatsal Agarwal <103558712+vatsalagarwall@users.noreply.github.com>
2025-07-31 12:46:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList.js';
 import ProductDetail from './components/ProductDetail';
+import DepartmentList from './components/DepartmentList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DepartmentProducts from './components/DepartmentProduct';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<ProductList />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/departments" element={<DepartmentList />} />
+      <Route path="/departments/:id" element={<DepartmentProducts />} />
+      <Route path="/departments/:id/:name" element={<ProductDetail />} />
+    </Routes>
   );
 }
 
